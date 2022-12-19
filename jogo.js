653300993e0466e2253d7715384cd651d2714650
ilipe <118455180+ilipehwd@users.noreply.github.com>
2022-12-19 15:31:56 -0300

    Aula 24

--- jogo.js
+++ jogo.js
@@ -1,4 +1,3 @@
-// Aqui teremos a programação do Flappy Bird :D
 const sprites = new Image();
 sprites.src = './sprites.png';
 
@@ -22,8 +21,16 @@ const flappyBird = {
                 flappyBird.x, flappyBird.y,
                 flappyBird.largura, flappyBird.altura,
             );
+        },
+        gravidade: 0.25,
+        velocidade: 0,
+        atualiza(){
+            flappyBird.Y = flappyBird.y + 1
+            flappyBird.velocidade += flappyBird.gravidade;
+            flappyBird.y = flappyBird.y + flappyBird.velocidade;
         }
 }
+
 const planodefundo = {
     spriteX: 390,
     spriteY: 0,
@@ -48,13 +55,85 @@ const planodefundo = {
             );
         }
 }
+const chao = {
+    spriteX: 0,
+    spriteY: 610,
+    largura: 224,
+    altura: 111,
+    x: 0,
+    y: canvas.height - 100,
+        desenha() {
+            contexto.drawImage(
+                sprites,
+                chao.spriteX, chao.spriteY,
+                chao.largura, chao.altura,
+                chao.x, chao.y,
+                chao.largura, chao.altura,
+            );
+            contexto.drawImage(
+                sprites,
+                chao.spriteX, chao.spriteY,
+                chao.largura, chao.altura,
+                chao.x + chao.largura, chao.y,
+                chao.largura, chao.altura,
+            );
+        }
+}
+const inicio = {
+    spriteX: 130,
+    spriteY: 0,
+    largura: 180,
+    altura: 152,
+    x: 70,
+    y: 70,
+        desenha() {
+            contexto.drawImage(
+                sprites,
+                inicio.spriteX, inicio.spriteY,
+                inicio.largura, inicio.altura,
+                inicio.x, inicio.y,
+                inicio.largura, inicio.altura,
+            );
+}}
+const telainicio = {
+    desenha(){
+        planodefundo.desenha();
+        chao.desenha();
+        flappyBird.desenha();
+        inicio.desenha();
+    },
+    click(){
+        telaativa = telajogo;
+    }
+}
+
+const telajogo = {
+    desenha(){
+        planodefundo.desenha();
+        chao.desenha();
+        flappyBird.desenha();
+        flappyBird.atualiza();
+    },
+    click(){},
+}
+
+var telaativa = telainicio;
+
+function mudatelaativa(){
+    telaativa.click();
+}
+
+window.addEventListener('click', mudatelaativa);
 
 function loop(){
-    contexto.fillStyle = '#00FFFF';
+    contexto. Style = '#00FFFF';
     contexto.fillRect(0,0, canvas.width, canvas.height)
     flappyBird.desenha();
     planodefundo.desenha();
+    chao.desenha();
+    telaativa.desenha();
     requestAnimationFrame(loop);
 }
+
 
 loop();
